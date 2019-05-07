4e672534118d5cc0c99ea1436f17f7c4ad79bb3c
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-07 14:41:05 +0100

    render project list and task list done 2

--- client/index.js
+++ client/index.js
@@ -1,8 +1,28 @@
-loginBtn = ...
+const renderProjectLi = (project) => {
+  const projectLi = document.createElement('li')
+  projectLi.id = `project-list${project.id}`
+  projectLi.innerText = `${project.name}`
+  document.body.append(projectLi)
+}
 
+const renderProjects = (projects) => {
+  projects.forEach(renderProjectLi)
+}
 
-loginBtn.addEventListener('submit', event =>{
-  event.preventDefault()
+const renderTaskLi = (task) => {
+  const taskLi = document.createElement('li')
+  taskLi.id = `task-list${task.id}`
+  taskLi.innerText = `${task.description}`
+  document.body.append(taskLi)
+}
 
-  formEl.username.value 
-})
+const renderTasks = (tasks) => {
+  tasks.forEach(renderTaskLi)
+}
+
+const init = () => {
+  renderProjects(USERS[1]["projects"])
+  renderTasks(USERS[1]["projects"][0]["tasks"])
+}
+
+init()
--- server/app/serializers/project_serializer.rb
+++
@@ -1,3 +0,0 @@
-class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :favourite_status, :archive_status, :user_id
-end
