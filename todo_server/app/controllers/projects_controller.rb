5965d227e4202341d4251bf00e997c2763a39cc1
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-07 12:36:48 +0100

    define json structure

--- todo_server/app/controllers/projects_controller.rb
+++ todo_server/app/controllers/projects_controller.rb
@@ -1,18 +1,20 @@
 class ProjectsController < ApplicationController
-    def show
-        project = Project.find_by(id: params[:id])
-        if project
-            render json: project
-        else
-            render json: {error: 'No project found'}, status: 404
-        end
+  def index
+    projects = Project.all
+    if projects
+        render json: projects
+    else
+        render json: {error: 'No project found'}, status: 404
     end
-      def index
-        projects = Project.all
-        if projects
-            render json: projects
-        else
-            render json: {error: 'No project found'}, status: 404
-        end
+  end
+
+    def show
+      project = Project.find_by(id: params[:id])
+      if project
+          render json: project
+      else
+          render json: {error: 'No project found'}, status: 404
+      end
     end
 end
+USER 
