7c30a68d5ca73e407ba2c488bbda495a89083e1d
sam20gh <sam20gh@gmail.com>
2019-05-09 18:02:50 +0100

    New task Form done

--- todo_server/app/controllers/tasks_controller.rb
+++ todo_server/app/controllers/tasks_controller.rb
@@ -18,6 +18,7 @@ class TasksController < ApplicationController
   end
 
   def create
+    params[:due_date] = params[:due_date].to_time
     task = Task.create(task_params)
     if task
         render json: task
